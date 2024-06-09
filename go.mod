88c699dd389a884dacd17cf11b48b64e47f9b1a3
BrendanWallaceNash98 <brendan.wallacenash@connect.qut.edu.au>
2024-06-09 18:39:00 +1000

    m

--- go.mod
+++ go.mod
@@ -1,1 +1,8 @@
 module goth_stack
+
+go 1.21.1
+
+require (
+	github.com/go-chi/chi/v5 v5.0.12
+	github.com/joho/godotenv v1.5.1
+)
---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"github.com/go-chi/chi/v5"
+	"github.com/joho/godotenv"
+	"log"
+	"log/slog"
+	"net/http"
+	"os"
+)
+
+func main() {
+	if err := initEverything(); err != nil {
+		log.Fatal(err)
+	}
+	router := chi.NewMux()
+
+	// router.Get("/",) {
+	port := os.Getenv("HTTP_LISTEN_ADDR")
+	slog.Info("application running", "port", port)
+	log.Fatal(http.ListenAndServe(port, router))
+
+}
+
+func initEverything() error {
+	return godotenv.Load()
+}
